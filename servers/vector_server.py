d00267d57b0cc20ad36b56dc4450d0eecbb8b58b
Gonzalo Zabala <78758188+GZabalaG@users.noreply.github.com>
2025-09-03 19:16:59 +0200

    Update vector_server.py

--- servers/vector_server.py
+++ servers/vector_server.py
@@ -20,7 +20,6 @@ logging.basicConfig(
 # Pinecone Init
 # ----------------------------
 PINECONE_API_KEY = (
-    "pcsk_4hANZE_Pj2J5QUcPLPPiL8WXZKzBMba2es7PMLyRaett6bq9QUiswrdE7953iN4sBN5BdB"
 )
 if not PINECONE_API_KEY:
     raise RuntimeError("❌ Missing Pinecone API key")
